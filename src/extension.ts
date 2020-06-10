f3250a80a2fccba0d3853598f585c4fb756366df
Stephen Friedman <stephen.friedman@deciphernow.com>
2020-06-10 02:15:53 -0400

    fixing typescript errors, upaded publisher name

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 
 'use strict'
-import { commands, ExtensionContext, window, workspace, Range, Position, TextEditorRevealType} from 'vscode'
+import { commands, ExtensionContext, window, workspace, Range, Position, TextEditorRevealType, TextEditorDecorationType} from 'vscode'
 
 export async function activate(context: ExtensionContext) {
     let cursorLine = defaultHighlight();
@@ -16,11 +16,11 @@ export async function activate(context: ExtensionContext) {
         } catch (error){
             console.error("Error from ' window.onDidChangeActiveTextEditor' -->", error)
         } finally {
-            lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+            lastActivePosition = new Position(activeEditor!.selection.active.line, activeEditor!.selection.active.character);
         }
     })
 
-    function setHighlight(cursorLine, updateAllVisibleEditors=false) {
+    function setHighlight(cursorLine: TextEditorDecorationType, updateAllVisibleEditors=false) {
         try {
             if (updateAllVisibleEditors) {
                 window.visibleTextEditors.forEach((editor) => {
@@ -44,7 +44,7 @@ export async function activate(context: ExtensionContext) {
         catch (error){
             console.error("Error from ' setHighlight' -->", error)
         } finally {
-            lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+            lastActivePosition = new Position(activeEditor!.selection.active.line, activeEditor!.selection.active.character);
         }
     }
 
