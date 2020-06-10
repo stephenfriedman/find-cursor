2c9248e402f748bbb39687cfbb008c081de63dbe
Stephen Friedman <stephen.friedman@deciphernow.com>
2020-06-10 01:45:08 -0400

    inital vscode extension commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# locatecursorline README
+
+When you do `Cmd+H` it will highlight the line where your cursor is clicked. Clicking anywhere or typing a key will remove the highlight.
+
---
+++ src/extension.ts
@@ -0,0 +1,91 @@
+
+'use strict'
+import { commands, ExtensionContext, window, workspace, Range, Position, TextEditorRevealType} from 'vscode'
+
+export async function activate(context: ExtensionContext) {
+    let cursorLine = defaultHighlight();
+    let activeEditor = window.activeTextEditor;
+    let lastActivePosition;
+    let lineStatus = false
+    window.onDidChangeActiveTextEditor(() => {
+        try {
+            activeEditor = window.activeTextEditor
+            cursorLine.dispose();
+            cursorLine = defaultHighlight();
+            lineStatus = false
+        } catch (error){
+            console.error("Error from ' window.onDidChangeActiveTextEditor' -->", error)
+        } finally {
+            lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+        }
+    })
+
+    function setHighlight(cursorLine, updateAllVisibleEditors=false) {
+        try {
+            if (updateAllVisibleEditors) {
+                window.visibleTextEditors.forEach((editor) => {
+                    const currentPosition = editor.selection.active;
+                    const currentLine = editor.selection.active.line;
+                    const newHighlight = { range: new Range(currentPosition, currentPosition) };
+                    editor.setDecorations(cursorLine, [newHighlight]);
+                });
+            }
+
+            else {
+                window.visibleTextEditors.forEach((editor) => {
+                    if(editor !== window.activeTextEditor) return;               
+                    const currentPosition = editor.selection.active
+                    const newHighlight = { range: new Range(currentPosition, currentPosition) }
+                    editor.revealRange( new Range(currentPosition, currentPosition),TextEditorRevealType.InCenterIfOutsideViewport)
+                    editor.setDecorations(cursorLine, [newHighlight])
+                });
+            }
+        } 
+        catch (error){
+            console.error("Error from ' setHighlight' -->", error)
+        } finally {
+            lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+        }
+    }
+
+
+    workspace.onDidChangeConfiguration(() => {
+        cursorLine.dispose();
+        cursorLine = defaultHighlight();
+        lineStatus = false
+    })
+
+    window.onDidChangeTextEditorSelection(() => {
+        activeEditor = window.activeTextEditor;   
+        cursorLine.dispose();
+        cursorLine = defaultHighlight();
+        lineStatus=false
+    })
+
+    let disposable = commands.registerCommand('locatecursor.helloWorld', () => {
+        if (lineStatus === false) {
+            lineStatus=true
+            setHighlight(cursorLine);           
+        } else {
+            lineStatus=false
+            cursorLine.dispose();
+            cursorLine = defaultHighlight();
+        }
+        
+	});
+	context.subscriptions.push(disposable);
+}
+
+
+function defaultHighlight() {
+    const cursorLine = window.createTextEditorDecorationType({
+        isWholeLine: true,
+        borderWidth: `2px 0 2px 0`,
+        borderStyle: "solid",
+        border: "solid",
+        borderColor: "#d400ff"
+    })
+    return cursorLine;
+}
+export function deactivate() {
+}
